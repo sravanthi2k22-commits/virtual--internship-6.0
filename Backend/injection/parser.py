9bdcc4bd39bee6235e13d3b90c34d29bc9b73b1d
Saikumar <saikumarpatel35@email.com>
2026-02-19 19:45:00 +0530

    Updated loader, parser and README

--- Backend/injection/parser.py
+++ Backend/injection/parser.py
@@ -16,7 +16,18 @@ LOG_PATTERN = re.compile(
     r"(?P<level>\w+)\s"
     r"(?P<service>\w+)\s"
     r"(?P<message>.*?)(?:\suser_id=(?P<user_id>\d+))?$"
+    
 )
+def parse_log_line(line):
+    match = LOG_PATTERN.match(line)
+    if not match:
+        return None
+    return {
+        "timestamp": match.group("timestamp"),
+        "level": match.group("level"),
+        "service": match.group("service"),
+        "message": match.group("message")
+    }
 #Groupdict()
 #Strip()
 #re.compile();recompile is a method in python which is used to create regex patterns
@@ -25,3 +36,11 @@ LOG_PATTERN = re.compile(
 #?->starting of pattern code
 #P<timestamp>,P<level>,P<service>,P<message> which is used to capture the names of particular data
 #\s -> used to remove white spaces between the name given
+'''groupdict() -> convert raw data into structure data (dictionary)
+    r'' -> raw Data
+    ?P<timestamp> -> named group
+    s+ -> for spaces one or more
+    ^ -> start of line
+    $ -> end of line
+    \S -> non space character
+   dask_bag=["2025-01-12 10:45:32 error auth failed to login","2025-01-12 10:45:32 error auth failed to login","2025-01-12 10:45:32 error auth failed to login"] '''
