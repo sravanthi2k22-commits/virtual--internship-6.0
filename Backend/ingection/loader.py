1b8c5baf06a478cb4a81d0f21c3be989fc5788ee
Saikumar <saikumarpatel35@email.com>
2026-03-03 18:44:01 +0530

    Auto update - 2026-03-03 18:44:01

--- Backend/ingection/loader.py
+++ Backend/ingection/loader.py
@@ -4,9 +4,4 @@ from backend.schema.schema import LOG_SCHEMA
 
 def load_logs(file_path):
     bag = db.read_text(file_path)
-    parsed = (
-        bag.map(parse_log_line)
-        .filter(lambda x: x is not None)  # Filter out lines that failed to parse
-    )
-    df = parsed.to_dataframe()
-    return df.astype(LOG_SCHEMA)
+    return bag
