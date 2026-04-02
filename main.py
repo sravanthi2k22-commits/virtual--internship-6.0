03df360be1897420dff93f69bed86b87b421b378
sravanthi2k22-commits <sravanthi2k22@gmail.com>
2026-04-02 11:27:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,61 @@
 import time
 from backend.config.dask_config import start_dask
 from backend.pipeline.processing import build_pipeline
-from backend.anomaly.detection import detect_anamoly
+from backend.anomaly.detection import detect_anomalies
 from backend.config.email_alert import send_anomaly_email
 
+
 def main():
+    # Start Dask cluster
     client = start_dask()
     print(client)
     print(f"Dashboard link: {client.dashboard_link}")
     print("\n" + "=" * 50)
+
     start = time.time()
+
     # Build log processing pipeline
-    log_df = build_pipeline(r"D:\log-analytics-monitoring-engine\backend\sample_log\data_log.log")
+    log_df = build_pipeline(r"backend\sample_log\data_log.log")
+
     print("Parsed Log Data (Table Format):")
     print(log_df.compute())
-    # Correct way to count rows
+
+    # Count total logs
     total_logs = log_df.shape[0].compute()
+
     end = time.time()
+
     print("Total logs parsed:", total_logs)
     print("Time taken:", round(end - start, 2), "seconds")
+
     print("\nRunning anomaly detection...")
+
     # Detect anomalies
-    anomalies = detect_anamoly(log_df)
-    # If still a Dask dataframe convert to pandas
+    anomalies = detect_anomalies(log_df)
+
+    # Convert Dask dataframe to Pandas if needed
     if hasattr(anomalies, "compute"):
         anomalies = anomalies.compute()
+
     if anomalies.empty:
         print("No anomalies detected")
     else:
         print(f"🚨 {len(anomalies)} anomalies detected!")
-        
+
         for minute, row in anomalies.iterrows():
             anomaly_data = {
                 "timestamp": minute,
-    "error_count": row.get("error_count", 0),
-    "z_score": row.get("z_score", 0)
+                "error_count": row.get("error_count", 0),
+                "z_score": row.get("z_score", 0)
             }
+
+            # Send email alert
             send_anomaly_email(
-sender_email="",
-password="",
- to_email="",
-anomaly=anomaly_data
-    )
+                sender_email="your_email@gmail.com",
+                password="your_app_password",
+                to_email="receiver_email@gmail.com",
+                anomaly=anomaly_data
+            )
 
             print(
                 f"Alert | Time: {minute} | "
