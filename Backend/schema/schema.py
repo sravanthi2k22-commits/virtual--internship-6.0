18b33a9a569ba5d395c7d4bfc269939dd2eb981e
sravanthi2k22-commits <sravanthi2k22@gmail.com>
2026-04-03 19:54:56 +0530

    schema.py

--- Backend/schema/schema.py
+++ Backend/schema/schema.py
@@ -1,8 +1,4 @@
-#schema files define the structure of data and type of the data , schema is nothing but role book of our data .
-# uses of schema files :
-#1. it ensures all rows follows the same structure and type of data 
-#2. it helps in data validation and data cleaning .
-#3. faster processing , easier analytics (flitering , mapping,grouping,anomaly detection becomes simple and faster)    
+
 LOG_SCHEMA = {
 "timestamp": "datetime64[ns]",
 "level": "string",
