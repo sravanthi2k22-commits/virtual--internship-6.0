745fd77864d0870b63b360fc486b09f08d65de8e
sravanthi2k22-commits <sravanthi2k22@gmail.com>
2026-04-03 19:31:41 +0530

    Update dask_config.py

--- Backend/config/dask_config.py
+++ Backend/config/dask_config.py
@@ -1,10 +1,10 @@
 from dask.distributed import Client, LocalCluster
+
 def start_dask():
     cluster = LocalCluster(
         n_workers=4,
         threads_per_worker=2,
-        memory_limit='1GB',
+        memory_limit="1GB",
         dashboard_address=":8790"
     )
-
     return Client(cluster)
