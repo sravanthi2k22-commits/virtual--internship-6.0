1362d0641f36b2395c6f1bc11404036e38d7ee16
sravanthi2k22-commits <sravanthi2k22@gmail.com>
2026-04-03 19:53:12 +0530

    processing.py

--- Backend/pipeline/processing.py
+++ Backend/pipeline/processing.py
@@ -1,25 +1,4 @@
-#why pipeline?
-#this pipeline injects the raw log data using dask bag. parse them into structured data with the help 
-# of dask data frame, we can filter the invalid log entries and converts into data frame using meta data.
 
-#why meta data?
-#dask cannot guess the column type automatically , 
-# we tell dask what is key and what is the value(passing key and value pairs) 
-# like what are column names and data types of the particular column.
-
-#why objects?
-#raw log data are strings intially ,
-# which are in text files, using objects it is safer browser for better understanding
-
-
-# example
-# meta_data = {
-#     # pandas/dask expects a valid dtype; use datetime64 for timestamp
-#     "timestamp": "datetime64[ns]", 
-#     "level" : "string", 
-#     "service" : "string", 
-#     "message" : "string",
-# }
 
 import dask.dataframe as dd
 from backend.ingection.parser import parse_log_line
